30f341b6fc8032a42b6ad5f4886dc523617ae6b9
Sam Ferwerda <sam.ferwerda@hotmail.com>
2018-06-12 18:09:00 +0200

    Included a function to visualize easier, cars are no. 1, empty = 0.

--- highway.py
+++ highway.py
@@ -15,6 +15,17 @@ class HighWay:
 				if(random.random() < self.density):
 					self.road[i,j] = Car(i,j)
 
+	# this function returns a matrix with zeros and ones. Every one is a car, such that
+	# we can plot it using imshow.
+	def visualize_road(self):
+		self.visualized_road = np.zeros((lanes, length))
+		for i in range(self.lanes):
+			for j in range(self.length):
+				if self.road[i,j] != None:
+					self.visualized_road[i,j] = 1
+
+		return self.visualized_road
+
 class Car:
 	def __init__(self, xpos, ypos):
 		self.xpos = xpos
